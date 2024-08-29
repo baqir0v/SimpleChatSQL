81dcf62c58ec215781cce2f37550a73aec48530a
Ahmad Baghirov <tu5r38eh9@code.edu.az>
2024-08-29 13:30:31 +0400

    gitignore

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,12 +3,12 @@ import { Sequelize } from "sequelize"
 dotenv.config()
 
 const sequelize = new Sequelize({
-    host:"localhost",
-    username:"root",
-    password:process.env.MYSQL_PASSWORD,
-    database:"admin_erp",
-    dialect:"mysql",
-    logging:false
+    host: "localhost",
+    username: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "admin_erp",
+    dialect: "mysql",
+    logging: false
 })
 
 export default sequelize
